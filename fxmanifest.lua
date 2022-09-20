d78b0bd258cf9ac97feb9b1757fc08fa59ac65fc
ChristianRosh <109095159+ChristianRosh@users.noreply.github.com>
2022-09-20 05:55:00 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+--Map By christianrosh#6659
+
+--Thank´s for download my map i hope your community enjoy it.
+
+--JustGameEnjoy Dev Team.
+
+-- https://discord.gg/YRxdNmW27u
